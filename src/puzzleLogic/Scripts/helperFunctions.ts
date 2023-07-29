07df123caea7488a3be4fc52fdffedf07ae2e8ee
Houk13 <45561058+Houk13@users.noreply.github.com>
2023-07-29 22:47:10 +0200

    Some refactoring + many sudoku helper functions

--- src/puzzleLogic/Scripts/helperFunctions.ts
+++ src/puzzleLogic/Scripts/helperFunctions.ts
@@ -3,7 +3,6 @@ type celValue = (number | "");
 export const isValidDigitNtoM = (num: celValue, M: number, N: number = 1): boolean => {
     return !(num === "" || num < N || num > M || num % 1 !== 0);
 }
-
 export const isSetNtoM = (set: celValue[], M: number, N:number = 1): boolean => {
     if (set.length !== M - N + 1) return false;
     const bitSum = Math.pow(2, M) - Math.pow(2, N - 1);
@@ -14,14 +13,4 @@ export const isSetNtoM = (set: celValue[], M: number, N:number = 1): boolean => 
     , 0);
 }
 
-export const getSudokuBox = (grid: celValue[][], box: number) : celValue[] => {
-    const boxArray: celValue[] = [];
-        const startRow = 3 * Math.floor(box / 3);
-        const startCol = 3 * ((box - 1) % 3);
-        for (let row = startRow; row !== startRow + 3; row++){
-            for (let col = startCol; col !== startCol + 3; row++){
-                boxArray.push(grid[row][col]);
-            }
-        }
-    return boxArray;
-}
+
