729f1e949f1350f73eed8fffe215af70b8226eb1
Houk13 <45561058+Houk13@users.noreply.github.com>
2023-07-31 23:28:34 +0200

    some debugging.

--- src/Sandbox.ts
+++ src/Sandbox.ts
@@ -10,9 +10,10 @@
 import Selection from "./UILogic/Selection";
 import { Options } from "./puzzleLogic/Classes/Options"
 import { isSetNtoM } from "./puzzleLogic/Scripts/helperFunctions";
+import * as sh from "./puzzleLogic/Scripts/sudokuHelper";
 import { sudoku } from "./puzzleLogic/Sudoku/TestSudoku";
 import generateQuickSudokuScan from "./puzzleLogic/Sudoku/generateQuickSudokuScan";
-
+import { celValue } from "./puzzleLogic/Scripts/sudokuHelper";
 // import { isSet1to9 } from "./puzzleLogic/Scripts/sudokuRules";
 
 // let file = fetch("test.txt").then(res => res.text()).then(res => console.log(res));
@@ -39,7 +40,23 @@ import generateQuickSudokuScan from "./puzzleLogic/Sudoku/generateQuickSudokuSca
 // let generator = generateQuickSudokuScan();
 // console.log(generator());
 
-console.log(isSetNtoM([2, 3.2, 4], 4, 2));
+const testSudoku: celValue[][] = 
+                   [["", "", "", 7 , "", 1 , "", "", ""],
+                    [7 , 3 , "", "", 9 , "", "", "", 2 ],
+                    [5 , 6 , 1 , "", "", "", 4 , 9 , ""],
+                    [4 , 7 , 2 , 3 , 1 , 8 , "", "", ""],
+                    ["", "", "", 5 , "", 9 , "", "", ""],
+                    ["", "", "", 4 , 6 , 7 , 2 , 8 , 3 ],
+                    ["", 4 , 6 , "", "", "", 1 , 5 , 9 ],
+                    [2 , "", "", "", 3 , "", "", 7 , 4 ],
+                    ["", "", "", 1 , "", 5 , "", "", ""]];
+
+// console.log(sh.getSudokuBox(testSudoku, 1))
+console.log(sh.getPositionRowOptions(testSudoku,4));  
+// console.log(sh.getSudokuRow(testSudoku, 1));
+// console.log(sh.getDirectBoxOptions(testSudoku, 1));
+// console.log(sh.getDirectRowOptions(testSudoku, 1));
+// console.log(sh.getDirectCelOptions(testSudoku, 5, 9))                  
 
 let list = [1, 2, 3, "hoi"];
 const sudokuOptionList = list;
--- src/puzzleLogic/Scripts/sudokuHelper.ts
+++ src/puzzleLogic/Scripts/sudokuHelper.ts
@@ -4,13 +4,16 @@
     Then the functions in this file can be specific versions of the 
     functions in that file.
 */
+
+
 // Where possible, use the existing helper functions.
 import * as hf from "./helperFunctions"; 
+const SUDOKUSIZE = 9;
 // 
 export type celValue = (number | "");
 
 /** checks whether the given array is a complete and unique set of the number 1 to 9 */
-export const isSet1to9 = (set: (number | "")[]): boolean => hf.isSetNtoM(set, 9);
+export const isSet1to9 = (set: (number | "")[]): boolean => hf.isSetNtoM(set, SUDOKUSIZE);
 
 // -------------Selection functions for sudoku
 /** determines the boxnumber based on the given row and column (count 1 to 9) */
@@ -20,28 +23,29 @@ export const getBoxNum = (row: number, col: number): number => {
     return (3 * tb) + lr + 1;
 }
 /** returns an array with the values in the given box */
-export const getSudokuBox = (grid: celValue[][], box: number): celValue[] => {
-    const boxArray: celValue[] = [];
-        const startRow = 3 * Math.floor(box / 3);
+export const getSudokuBox= <T,>(grid: T[][], box: number): T[] => {
+    const boxArray: T[] = [];
+        const startRow = 3 * Math.floor((box - 1) / 3);
         const startCol = 3 * ((box - 1) % 3);
         for (let row = startRow; row !== startRow + 3; row++){
-            for (let col = startCol; col !== startCol + 3; row++){
+            for (let col = startCol; col !== startCol + 3; col++){
+                // console.log(row, col);
                 boxArray.push(grid[row][col]);
             }
         }
     return boxArray;
 }
 /** returns an array with the values in the given row */
-export const getSudokuRow = (grid: celValue[][], row: number): celValue[] => {
-    return grid[row];
+export const getSudokuRow = <T,>(grid: T[][], row: number): T[] => {
+    return grid[row - 1];
 }
 /** returns an array with the values in the given column */
-export const getSudokuCol = (grid: celValue[][], col: number): celValue[] => {
+export const getSudokuCol = <T,>(grid: T[][], col: number): T[] => {
     return grid.map(row => row[col - 1]);
 }
 /** returns the highest repeated number, or 0 if there are no repeats */
 export const hasRepeatedNumber = (arr: celValue[]): number => {
-    const lib = new Array(9).fill(0);
+    const lib = new Array(SUDOKUSIZE).fill(0);
     let double = 0;
     arr.forEach(val => {
         if (val === "") return;
@@ -55,7 +59,7 @@ export const hasRepeatedNumber = (arr: celValue[]): number => {
 }
 /** counts which values are present in the given array, returns an array of length 9 */
 export const getValueCount = (arr: celValue[]): number[] => {
-    const lib = new Array(9).fill(0);
+    const lib = new Array(SUDOKUSIZE + 1).fill(0);
     arr.forEach(val => {
         if (val === "") return;
         lib[val]++;
@@ -86,8 +90,8 @@ export const getDirectColOptions = (grid: celValue[][], col: number): number[] =
 }
 /** returns an array of the values not used yet in the given box (can be empty) */
 export const getDirectBoxOptions = (grid: celValue[][], box: number): number[] => {
-    const lib = getValueCount(getSudokuRow(grid, box));
-    const options = [];
+    const lib = getValueCount(getSudokuBox(grid, box));
+    const options: number[] = [];
     for (let opt = 1; opt !== 10; opt++) {
         if (lib[opt] === 0) {
             options.push(opt);
@@ -101,11 +105,11 @@ export const getDirectCelOptions = (grid: celValue[][], row: number, col: number
     const colOpts = getDirectColOptions(grid, col);
     const boxOpts = getDirectBoxOptions(grid, getBoxNum(row, col));
     // cel options need to be in all three optionlists;
-    const lib = new Array(9).fill(0);
+    const lib = new Array(SUDOKUSIZE + 1).fill(0);
     const celOpts = [];
-    for (let opt in rowOpts){lib[opt]++}
-    for (let opt in colOpts){lib[opt]++}
-    for (let opt in boxOpts){lib[opt]++}
+    for (let opt of rowOpts){lib[opt]++}
+    for (let opt of colOpts){lib[opt]++}
+    for (let opt of boxOpts){lib[opt]++}
     for (let opt = 1; opt !== 10; opt++){
         if (lib[opt] === 3) celOpts.push(opt);
     }
@@ -116,15 +120,32 @@ export const getDirectCelOptions = (grid: celValue[][], row: number, col: number
  * INEFFICIENT!!!
 */
 export const getDirectOptionGrid = (grid: celValue[][]): number[][][] => {
-    const optGrid: number[][][] = new Array(9);
-    for (let rownum: number = 0; rownum !== 9; rownum++){
-        let row = new Array(9);
-        for (let colnum: number = 0; colnum !== 9; colnum++){
-            row[colnum] = getDirectCelOptions(grid, rownum, colnum);
+    const optGrid: number[][][] = new Array(SUDOKUSIZE);
+    for (let rownum: number = 1; rownum !== SUDOKUSIZE + 1; rownum++){
+        let row = new Array(SUDOKUSIZE);
+        for (let colnum: number = 1; colnum !== SUDOKUSIZE + 1; colnum++){
+            row[colnum - 1] = (grid[rownum - 1][colnum - 1] !== "") ? 
+                [] : getDirectCelOptions(grid, rownum, colnum);
         }
-        optGrid[rownum] = row;
+        optGrid[rownum - 1] = row;
     }
     return optGrid;
+}
+
+export const directToPosOptions = (dirOptions: number[][]): number[][] => {
+    const posOptions = new Array(SUDOKUSIZE).fill([]);
+    dirOptions.forEach((cel, idx) => {
+        for (const opt of cel){
+            posOptions[opt - 1].push(idx + 1);
+        }
+    });
+    return posOptions;
+}
+
+export const getPositionRowOptions = (grid: celValue[][], row: number, optionGrid?: number[][][]): number[][] => {
+    optionGrid = optionGrid ?? getDirectOptionGrid(grid); // make the direct option grid if not given
+    const directRow = getSudokuRow(optionGrid, row);
+    return directToPosOptions(directRow);
 }
 /**  creates a grid like directoptions, but instead of the options for each cell
  *   it lists the options for each digit in that row. 
